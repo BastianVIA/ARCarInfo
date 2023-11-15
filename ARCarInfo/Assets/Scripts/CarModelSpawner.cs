ce0d53c806c901a19e0982cae61a4fd351c0db9e
BastianVIA <305294@VIAUC.dk>
2023-11-15 09:35:38 +0100

    Initial commit
    Co-Authored-By: Nissen99 <47100802+nissen99@users.noreply.github.com>

---
+++ ARCarInfo/Assets/Scripts/CarModelSpawner.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CarModelSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectPrefab;
+    
+    [SerializeField]
+    TextMeshProUGUI spawnText;
+    
+    [SerializeField]
+    ARTrackedImageManager trackedImageManager;
+
+    void OnEnable() => trackedImageManager.trackedImagesChanged += OnChanged;
+
+    void OnDisable() => trackedImageManager.trackedImagesChanged -= OnChanged;
+
+    void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var newImage in eventArgs.added)
+        {
+            UpdateTrackingText("Yes");
+            SpawnObject(newImage.transform.position);
+        }
+
+        foreach (var updatedImage in eventArgs.updated)
+        {
+            
+        }
+
+        foreach (var removedImage in eventArgs.removed)
+        {
+            
+        }
+    }
+    
+    void SpawnObject(Vector3 position)
+    {
+        GameObject spawnedObject = Instantiate(objectPrefab, position, Quaternion.identity);
+        spawnedObject.SetActive(true);
+    }
+
+    void UpdateTrackingText(string newText)
+    {
+        if (spawnText != null)
+        {
+            spawnText.text = newText;
+        }
+    }
+}
